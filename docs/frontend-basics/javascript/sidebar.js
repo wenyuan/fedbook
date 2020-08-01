44c0721d0ece078cc9951d16c65cb7c72fc545ad
wenyuan <xuwenyuan2016@163.com>
2020-08-01 23:03:06 +0800

    docs: add JavaScript series: data-types

---
+++ docs/frontend-basics/javascript/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'JavaScript',
+		path: '/frontend-basics/javascript/',
+    collapsable: false,
+    children: [
+      {
+        title: '一、数据类型',
+        path: '/frontend-basics/javascript/data-types',
+        collapsable: true
+      }
+    ]
+  }
+]
