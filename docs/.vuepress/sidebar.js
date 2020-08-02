e6970735cd3db049c1a284e8a687771a3c9d7661
wenyuan <xuwenyuan2016@163.com>
2020-08-02 10:47:16 +0800

    feat: add index page

--- docs/.vuepress/sidebar.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '/frontend-basics/html': require('../frontend-basics/html/sidebar'),
-  '/frontend-basics/css': require('../frontend-basics/css/sidebar'),
-  '/frontend-basics/javascript': require('../frontend-basics/javascript/sidebar')
-}
--- docs/frontend-basics/README.md
+++
@@ -1,3 +0,0 @@
-HTML：结构层
-CSS：表现层
-JavaScript：行为层
