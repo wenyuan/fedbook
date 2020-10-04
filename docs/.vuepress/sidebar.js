577194a02cdd36cc513694eee67693c0173b0ed5
wenyuan <xuwenyuan2016@163.com>
2020-10-04 15:48:13 +0800

    fix: warn: "没有有效的侧边栏配置"

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,7 +1,9 @@
 module.exports = {
+  '/': [],
   '/frontend-basics/html': require('../frontend-basics/html/sidebar'),
   '/frontend-basics/css': require('../frontend-basics/css/sidebar'),
   '/frontend-basics/javascript': require('../frontend-basics/javascript/sidebar'),
   '/interview': require('../interview/sidebar'),
-  '/frontend-frameworks/vuejs': require('../frontend-frameworks/vuejs/sidebar')
+  '/frontend-frameworks/vuejs': require('../frontend-frameworks/vuejs/sidebar'),
+  '/history': require('../history/sidebar')
 };
