328fd1879bbeeb3a385b62dd4ace7ac28d85e414
wenyuan <xuwenyuan2016@163.com>
2020-10-05 18:05:11 +0800

    fix: sidebar url泛匹配，造成页面无法显示目录

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '/': [],
   '/frontend-basics/html': require('../frontend-basics/html/sidebar'),
   '/frontend-basics/css': require('../frontend-basics/css/sidebar'),
   '/frontend-basics/javascript': require('../frontend-basics/javascript/sidebar'),
   '/interview': require('../interview/sidebar'),
   '/frontend-frameworks/vuejs': require('../frontend-frameworks/vuejs/sidebar'),
-  '/history': require('../history/sidebar')
+  '/history': require('../history/sidebar'),
+  '/': [],
 };
