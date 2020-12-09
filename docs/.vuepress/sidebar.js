3b060edf947dde8d52562d446c3870d0e8e92794
wenyuan <xuwenyuan2016@163.com>
2020-12-09 23:38:46 +0800

    docs(coding-style): add introduce

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,4 +1,5 @@
 module.exports = {
+  '/coding-style': require('../coding-style/sidebar'),
   '/frontend-basics/html': require('../frontend-basics/html/sidebar'),
   '/frontend-basics/css': require('../frontend-basics/css/sidebar'),
   '/frontend-basics/javascript': require('../frontend-basics/javascript/sidebar'),
---
+++ docs/coding-style/sidebar.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    title: '代码风格',
+    path: '/coding-style/',
+    collapsable: false,
+    children: []
+  }
+]
