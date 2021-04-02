41edc1f9662760c4b0650aa70bd004899c70844a
wenyuan <xuwenyuan2016@163.com>
2021-04-02 15:59:35 +0800

    docs(handwritten): clean sidebar

--- docs/handwritten/javascript/sidebar.js
+++ docs/handwritten/javascript/sidebar.js
@@ -6,52 +6,52 @@ module.exports = [
     children: [
       {
         title: '实现防抖函数（debounce）',
-        path: '/handwritten/javascript/1-实现防抖函数-debounce',
+        path: '/handwritten/javascript/实现防抖函数-debounce',
         collapsable: true
       },
       {
         title: '实现节流函数（throttle）',
-        path: '/handwritten/javascript/2-实现节流函数-throttle',
+        path: '/handwritten/javascript/实现节流函数-throttle',
         collapsable: true
       },
       {
         title: '实现浅克隆（shallowClone）',
-        path: '/handwritten/javascript/3-实现浅克隆-shallow-clone',
+        path: '/handwritten/javascript/实现浅克隆-shallow-clone',
         collapsable: true
       },
       {
         title: '实现深克隆（deepClone）',
-        path: '/handwritten/javascript/4-实现深克隆-deep-clone',
+        path: '/handwritten/javascript/实现深克隆-deep-clone',
         collapsable: true
       },
       {
         title: '实现 instanceof 运算符',
-        path: '/handwritten/javascript/5-实现instanceof运算符',
+        path: '/handwritten/javascript/实现instanceof运算符',
         collapsable: true
       },
       {
         title: '实现 Object.create 方法',
-        path: '/handwritten/javascript/6-实现object-create方法',
+        path: '/handwritten/javascript/实现object-create方法',
         collapsable: true
       },
       {
         title: '实现 new 运算符',
-        path: '/handwritten/javascript/7-实现new运算符',
+        path: '/handwritten/javascript/实现new运算符',
         collapsable: true
       },
       {
         title: '实现 call 方法',
-        path: '/handwritten/javascript/8-实现call方法',
+        path: '/handwritten/javascript/实现call方法',
         collapsable: true
       },
       {
         title: '实现 apply 方法',
-        path: '/handwritten/javascript/9-实现apply方法',
+        path: '/handwritten/javascript/实现apply方法',
         collapsable: true
       },
       {
         title: '实现 bind 方法',
-        path: '/handwritten/javascript/10-实现bind方法',
+        path: '/handwritten/javascript/实现bind方法',
         collapsable: true
       },
       {
