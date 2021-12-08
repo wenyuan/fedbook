d2b4142ff0871329a05ed5866ae4c6031700795d
wenyuan <xuwenyuan2016@163.com>
2021-12-08 17:58:03 +0800

    docs(style-guide): add article: tob

--- docs/style-guide/product/sidebar.js
+++ docs/style-guide/product/sidebar.js
@@ -2,6 +2,12 @@ module.exports = [
   {
     title: '文案指南',
     collapsable: false,
-    children: []
+    children: [
+      {
+        title: 'B 端产品文案指南',
+        path: '/style-guide/product/tob',
+        collapsable: true
+      }
+    ]
   }
 ]
