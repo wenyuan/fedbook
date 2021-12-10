556ce40d719030776053f1624d7b6b8720bc4280
wenyuan <xuwenyuan2016@163.com>
2021-12-10 10:38:51 +0800

    chore(git): bulk change article titles

--- docs/basic-skills/git/sidebar.js
+++ docs/basic-skills/git/sidebar.js
@@ -20,47 +20,47 @@ module.exports = [
     collapsable: false,
     children: [
       {
-        title: '问题：clone 速度过慢影响效率',
+        title: 'clone 速度过慢影响效率',
         path: '/basic-skills/git/solution-to-clone-too-slow',
         collapsable: true
       },
       {
-        title: '问题：commit 信息写错想要修改',
+        title: 'commit 信息写错想要修改',
         path: '/basic-skills/git/solution-to-commit-info-mistake',
         collapsable: true
       },
       {
-        title: '问题：刚刚提交的代码，发现写错了怎么办',
+        title: '刚刚提交的代码，发现写错了',
         path: '/basic-skills/git/solution-to-last-commit-code-mistake',
         collapsable: true
       },
       {
-        title: '问题：刚刚提交完，发现漏提交了文件',
+        title: '刚刚提交完，发现漏提交了文件',
         path: '/basic-skills/git/solution-to-missed-file-in-last-commit',
         collapsable: true
       },
       {
-        title: '问题：刚刚提交错了，想要撤回并重新提交',
+        title: '刚刚提交错了，想要撤回并重新提交',
         path: '/basic-skills/git/solution-to-withdraw-last-commit',
         collapsable: true
       },
       {
-        title: '问题：pull 时发现代码冲突，如何妥善解决',
+        title: 'pull 时发现代码冲突，如何妥善解决',
         path: '/basic-skills/git/solution-to-code-conflict',
         collapsable: true
       },
       {
-        title: '问题：如何修改历史 commits 中的用户名和邮箱',
+        title: '如何修改历史 commits 中的用户名和邮箱',
         path: '/basic-skills/git/solution-to-change-name-and-email-in-history-commits',
         collapsable: true
       },
       {
-        title: '问题：如何迁移仓库并保留 commits 记录',
+        title: '如何迁移仓库并保留 commits 记录',
         path: '/basic-skills/git/solution-to-migrate-repository-without-losing-history-commits',
         collapsable: true
       },
       {
-        title: '问题：如何参与开源项目 - 提交 PR 与更新 Fork 分支',
+        title: '如何参与开源项目 - 提交 PR 与更新 Fork 分支',
         path: '/basic-skills/git/solution-to-participate-in-open-source-projects',
         collapsable: true
       }
