2e395409f653276a8357913f3cf1fd7d3a27010a
wenyuan <xuwenyuan2016@163.com>
2025-09-21 14:00:11 +0800

    core: use pnpm

--- docs/frontend/package/sidebar.js
+++ docs/frontend/package/sidebar.js
@@ -6,7 +6,7 @@ module.exports = [
       {
         title: 'Node.js 安装及 npm 全局设置',
         path: '/frontend/package/nodejs-install-and-npm-global-config'
-      }
+      },
       {
         title: 'npm、yarn、pnpm 区别',
         path: '/frontend/package/npm-yarn-pnpm'
